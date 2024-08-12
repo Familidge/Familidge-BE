e83765f220eb48c2cbef75fa18a4baca5d4969ac
earlgrey02 <san06036@naver.com>
2024-08-12 21:32:38 +0900

    Add: R2DBC 관련 의존성 추가

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
     implementation(project(":application"))
     implementation(project(":domain"))
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation(libs.r2dbc.mysql)
 }
