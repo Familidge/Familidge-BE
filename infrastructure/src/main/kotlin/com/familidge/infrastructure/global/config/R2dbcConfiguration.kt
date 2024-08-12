1e752ad57dc61f5b275e90fa83948a9caf9f70a7
earlgrey02 <san06036@naver.com>
2024-08-12 21:33:14 +0900

    Feat: Auditing을 설정하는 R2DBC 설정 클래스 추가

---
+++ infrastructure/src/main/kotlin/com/familidge/infrastructure/global/config/R2dbcConfiguration.kt
@@ -0,0 +1,8 @@
+package com.familidge.infrastructure.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
+
+@EnableR2dbcAuditing
+@Configuration
+class R2dbcConfiguration
