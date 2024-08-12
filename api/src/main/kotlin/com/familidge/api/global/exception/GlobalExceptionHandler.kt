8bc177aaf1db0caec0db838d3737d7d5491093dc
earlgrey02 <san06036@naver.com>
2024-08-12 21:15:15 +0900

    Feat: 예외 처리 시 로깅 구현

--- api/src/main/kotlin/com/familidge/api/global/exception/GlobalExceptionHandler.kt
+++ api/src/main/kotlin/com/familidge/api/global/exception/GlobalExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.familidge.api.global.exception
 
 import com.familidge.api.global.dto.ErrorResponse
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.MediaType
@@ -9,14 +10,19 @@ import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
+import kotlin.reflect.jvm.jvmName
 
 @Component
 class GlobalExceptionHandler(
     private val objectMapper: ObjectMapper
 ) : ErrorWebExceptionHandler {
+    private val logger = KotlinLogging.logger(this::class.jvmName)
+
     override fun handle(exchange: ServerWebExchange, exception: Throwable): Mono<Void> =
         with(exchange.response) {
             val response = ErrorResponse(exception)
+
+            logger.error { "${exception::class.simpleName}(\"${exception.message}\") at ${exception.stackTrace[0]}" }
 
             headers.contentType = MediaType.APPLICATION_JSON
             statusCode = HttpStatusCode.valueOf(response.code)
