012ad065fbad74dae825ff32befb45401e6da6ac
earlgrey02 <san06036@naver.com>
2024-08-19 02:32:03 +0900

    Add: infrastructure 모듈에 Spring WebFlux 의존성 추가

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation(project(":application"))
     implementation(project(":domain"))
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation(libs.r2dbc.mysql)
 }
