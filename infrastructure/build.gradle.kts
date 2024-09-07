596c4827c5b82c187e73e7b54ad58c1502490e58
earlgrey02 <san06036@naver.com>
2024-09-07 18:07:18 +0900

    Add: Spring Data Reactive Redis 의존성 추가

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -3,5 +3,6 @@ dependencies {
     implementation(project(":domain"))
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation(libs.r2dbc.mysql)
 }
