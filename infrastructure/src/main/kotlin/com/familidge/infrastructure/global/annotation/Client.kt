36cd58eabf63bd6e15b9362e1c43f99c17c83a0c
earlgrey02 <san06036@naver.com>
2024-09-07 18:13:28 +0900

    Feat: Client 어노테이션 구현

---
+++ infrastructure/src/main/kotlin/com/familidge/infrastructure/global/annotation/Client.kt
@@ -0,0 +1,8 @@
+package com.familidge.infrastructure.global.annotation
+
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Component
+annotation class Client
