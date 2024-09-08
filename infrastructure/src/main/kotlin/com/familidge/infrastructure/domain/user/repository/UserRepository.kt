e5a0646b4ef25b8b597a049faccfe50aaff62f2f
earlgrey02 <san06036@naver.com>
2024-09-08 15:51:45 +0900

    Feat: UserRepository 구현

---
+++ infrastructure/src/main/kotlin/com/familidge/infrastructure/domain/user/repository/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.familidge.infrastructure.domain.user.repository
+
+import com.familidge.infrastructure.domain.user.entity.UserEntity
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : CoroutineCrudRepository<UserEntity, Int>
