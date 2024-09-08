19bba15ab2db0dab75516819d646b1c856e9ecf2
earlgrey02 <san06036@naver.com>
2024-09-08 17:24:04 +0900

    Refactor: 불필요한 isolationMode 설정 삭제

--- api/src/testFixtures/kotlin/com/familidge/api/config/ProjectConfiguration.kt
+++ api/src/testFixtures/kotlin/com/familidge/api/config/ProjectConfiguration.kt
@@ -2,13 +2,11 @@ package com.familidge.api.config
 
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.extensions.Extension
-import io.kotest.core.spec.IsolationMode
 import io.kotest.extensions.spring.SpringExtension
 import org.springframework.boot.test.context.TestConfiguration
 
 @TestConfiguration
 class ProjectConfiguration : AbstractProjectConfig() {
-    override val isolationMode = IsolationMode.InstancePerLeaf
     override val parallelism = 4
 
     override fun extensions(): List<Extension> = listOf(SpringExtension)
