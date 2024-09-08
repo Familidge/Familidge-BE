fc1439ae4cedc96136382eaab90427f83e998cd5
earlgrey02 <san06036@naver.com>
2024-09-08 17:26:25 +0900

    Add: 모듈간 테스트 픽스처 의존성 추가

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -2,4 +2,6 @@ dependencies {
     implementation(project(":domain"))
     implementation("org.springframework:spring-context")
     implementation(libs.bundles.jwt)
+    testImplementation(testFixtures(project(":domain")))
+    testFixturesImplementation(testFixtures(project(":domain")))
 }
